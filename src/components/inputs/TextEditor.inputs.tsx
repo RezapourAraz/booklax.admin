213d155d28b98cd0d5d476dd4512e5247776597f
ArazRezapour <rezapouraraz@gmail.com>
2024-02-18 13:53:55 +0330

    working on text editor

---
+++ src/components/inputs/TextEditor.inputs.tsx
@@ -0,0 +1,116 @@
+import React, { FC, useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+
+type ITextEditorInput = {
+  value: any;
+  onChange: any;
+};
+
+const TextEditorInput: FC<ITextEditorInput> = ({ value, onChange }) => {
+  const editorRef = useRef<any>(null);
+
+  return (
+    <Editor
+      apiKey="x7e5npbnun1h6db5gye2trbco63i0h45ah6278rnm5dcmxto"
+      onInit={(evt, editor) => (editorRef.current = editor)}
+      value={value}
+      onEditorChange={onChange}
+      init={{
+        directionality: "rtl",
+        height: 500,
+        width: "100%",
+        menubar: true,
+
+        plugins: [
+          "advlist",
+          "autolink",
+          "lists",
+          "link",
+          "image",
+          "charmap",
+          "preview",
+          "anchor",
+          "searchreplace",
+          "visualblocks",
+          "code",
+          "fullscreen",
+          "insertdatetime",
+          "media",
+          "table",
+          "help",
+          "wordcount",
+          "directionality",
+        ],
+        // @ts-ignore
+        selector: "textarea", // change this value according to your HTML
+        mobile: {
+          toolbar_drawer: "floating",
+        },
+        file_picker_types: "file image media",
+
+        file_picker_callback: function (cb, value, meta) {
+          var input = document.createElement("input");
+          input.setAttribute("type", "file");
+          input.setAttribute("accept", "image/*");
+          var url = `http://localhost:5005/api/v1/gallery`;
+          var xhr = new XMLHttpRequest();
+          var fd = new FormData();
+          xhr.open("POST", url, true);
+
+          input.onchange = function () {
+            // @ts-ignore
+            var file = this.files[0];
+
+            var reader = new FileReader();
+
+            xhr.onload = function () {
+              if (xhr.readyState === 4 && xhr.status === 201) {
+                console.log("yes");
+
+                // File uploaded successfully
+                var response = JSON.parse(xhr.responseText);
+
+                var url = `https://arazdev.storage.iran.liara.space//api/v1/gallery/${response?.src[0].image}`;
+                // Create a thumbnail of the uploaded image, with 150px width
+                cb(url, { title: file?.name });
+              }
+            };
+
+            reader.onload = function () {
+              var id = "blobid" + new Date().getTime();
+              var blobCache =
+                // @ts-ignore
+                window.tinymce.activeEditor.editorUpload.blobCache;
+              // @ts-ignore
+              var base64 = reader.result.split(",")[1];
+
+              var blobInfo = blobCache.create(id, file, base64);
+              blobCache.add(blobInfo);
+
+              // call the callback and populate the Title field with the file name
+
+              fd.append("file", blobInfo.blob());
+
+              xhr.send(fd);
+            };
+
+            reader.readAsDataURL(file);
+          };
+
+          input.click();
+        },
+        toolbar:
+          "undo redo | blocks | " +
+          "bold italic backcolor | alignleft aligncenter " +
+          "alignright alignjustify | bullist numlist outdent indent | " +
+          "removeformat | image" +
+          "ltr rtl",
+
+        content_style:
+          "@import url('https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700,900&display=swap'); @import url('https://v1.fontapi.ir/css/Vazir'); body { font-family:Vazir, Arial,sans-serif; font-size:16px, direction: rtl }",
+      }}
+    />
+  );
+};
+
+export default TextEditorInput;
--- src/pages/blogs/CreateBlog.tsx
+++ src/pages/blogs/CreateBlog.tsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Mui
-import { Button, Grid, Input, Typography } from "@mui/material";
+import { Button, Grid, Input, Typography, styled } from "@mui/material";
+
+import { Editor } from "@tinymce/tinymce-react";
+import TextEditorInput from "../../components/inputs/TextEditor.inputs";
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 const CreateBlog = () => {
+  // state
+  const [image, setImage] = useState<null | File>(null);
+  const [content, setContent] = useState();
+
+  // handle
+  const handleFile = (e: any) => {
+    setImage(e.target.files[0]);
+  };
+
+  const handleChangeText = (e: any) => {
+    setContent(e);
+  };
+
+  console.log(image);
+
   return (
     <Grid sx={{ p: 2, bgcolor: "common.white", borderRadius: 2 }}>
       <Grid sx={{ p: 2 }}>
@@ -26,13 +56,23 @@ const CreateBlog = () => {
         </Grid>
         <Grid item md={6} sx={{ my: 1, p: 2, alignItems: "center" }}>
           <Typography sx={{ p: 1 }}>بند انگشتی</Typography>
-          <Input
+          <Button
             fullWidth
-            disableUnderline
-            sx={{ px: 1, p: 0.5, bgcolor: "primary.light", color: "grey.600" }}
-            type="file"
-          />
+            component="label"
+            role={undefined}
+            tabIndex={-1}
+            sx={{ p: image ? 1 : 2.5 }}
+          >
+            {image ? image?.name : ""}
+            <VisuallyHiddenInput type="file" onChange={handleFile} />
+          </Button>
         </Grid>
+      </Grid>
+      <Grid>
+        <TextEditorInput
+          value={content}
+          onChange={(e: any) => handleChangeText(e)}
+        />
       </Grid>
       <Grid sx={{ display: "flex", justifyContent: "flex-end", p: 2 }}>
         <Button variant="contained" sx={{ boxShadow: 0 }}>
